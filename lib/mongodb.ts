d51b91e824fb9344b0cd9f2c7281b189ad308f5f
Al Zadid Yusuf <139882679+Blaze414@users.noreply.github.com>
2025-04-07 19:22:08 +0600

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -12,13 +12,13 @@ let clientPromise: Promise<MongoClient>;
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so the MongoClient is not constantly created
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(uri || '', options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
   // In production mode, create a new client for each request
-  client = new MongoClient(uri, options);
+  client = new MongoClient(uri || '', options);
   clientPromise = client.connect();
 }
 
