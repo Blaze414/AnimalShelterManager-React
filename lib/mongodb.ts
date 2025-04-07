3124bedfb17bf4471909c40c918035c106218cb7
Al Zadid Yusuf <139882679+Blaze414@users.noreply.github.com>
2025-04-07 19:36:27 +0600

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,30 +1,30 @@
 import { MongoClient } from 'mongodb';
 
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please add your Mongo URI to .env.local');
+}
+
 const uri = process.env.MONGODB_URI;
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
 if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable so the MongoClient is not constantly created
+  // In development mode, use a global variable so the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri || '', options);
+    client = new MongoClient(uri);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  // In production mode, create a new client for each request
-  client = new MongoClient(uri || '', options);
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri);
   clientPromise = client.connect();
 }
 
-// TypeScript declaration for global variable
-declare global {
-  var _mongoClientPromise: Promise<MongoClient>;
-}
-
-export default clientPromise; 
+export default clientPromise;
