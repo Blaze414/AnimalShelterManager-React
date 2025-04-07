2b0f7d60f71454fe1ebba198dd27e7527d7a0992
Al Zadid Yusuf <139882679+Blaze414@users.noreply.github.com>
2025-04-07 19:33:18 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['placeholder.com', 'your-actual-image-domain.com'],
-  },
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on'
-          },
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains'
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin'
-          }
-        ]
-      }
-    ]
-  }
-}
-
-module.exports = nextConfig 
+import type { NextConfig } from "next";
+ 
+ const nextConfig: NextConfig = {
+   /* config options here */
+ };
+ 
+ export default nextConfig;
